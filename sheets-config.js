a643cc21d222be8520d2ed8962225b2911f3ee4c
fih-seattle <163983286+peculab@users.noreply.github.com>
2026-09-15 13:17:19 -0700

    count app

--- sheets-config.js
+++ sheets-config.js
@@ -1,2 +1,2 @@
 // Paste the deployed Google Apps Script Web App URL here. Empty keeps FormSubmit active.
-window.BRIDGE_SHEETS_WEB_APP_URL = "";
+window.BRIDGE_SHEETS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyhn9dQ0_M5rmn46jv_PasokHw_oH1nTaUmndt78A7uJvQt_LigAVzg11qeNE-od1DWKg/exec";
